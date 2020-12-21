d07dd2b4d64d7066518e9a87767cbcbfd1ced93f
Jessdev <61420136+JessimH@users.noreply.github.com>
2020-12-21 21:15:05 +0100

    Add files via upload

---
+++ Front/src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBwN5qLxItL8RIbnSr_bi-pzIfmPDDgmeY",
+    authDomain: "whatsapp-mern-e493e.firebaseapp.com",
+    projectId: "whatsapp-mern-e493e",
+    storageBucket: "whatsapp-mern-e493e.appspot.com",
+    messagingSenderId: "361557624691",
+    appId: "1:361557624691:web:1e4093de53e410dfb2a706",
+    measurementId: "G-LXDSDD0JBL"
+};
+
